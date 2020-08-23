28620a73f9a4689eb36a402dda98bf1481a963bd
Devon Govett <devongovett@gmail.com>
2020-08-23 12:20:18 -0700

    Add dismiss button for screen readers

--- src/components/ListBox.vue
+++ src/components/ListBox.vue
@@ -2,7 +2,7 @@
 import {useListBox} from '@react-aria/listbox';
 import {wrap, useRef, useLayoutEffect} from '../react';
 import { mergeProps } from '@react-aria/utils';
-import {useOverlay} from '@react-aria/overlays';
+import {useOverlay, useDismissButton} from '@react-aria/overlays';
 import {useFocusScope} from '@react-aria/focus';
 import Option from './Option';
 import {ref} from 'vue';
@@ -47,10 +47,13 @@ export default {
 
     useFocusScope(reactOverlayRef, {restoreFocus: true});
 
+    let {dismissButtonProps} = useDismissButton({onDismiss: state.close});
+
     return {
       state,
       overlayProps,
       listBoxProps: mergeProps(listBoxProps, domProps),
+      dismissButtonProps,
       ref: vueRef,
       overlayRef: vueOverlayRef
     };
@@ -60,6 +63,7 @@ export default {
 
 <template>
   <div v-bind="overlayProps" class="absolute mt-1 w-full rounded-md bg-white shadow-lg z-50" ref="overlayRef">
+    <button v-bind="dismissButtonProps" />
     <ul ref="ref" v-bind="listBoxProps" class="max-h-56 rounded-md py-1 text-base leading-6 shadow-xs overflow-auto focus:outline-none sm:text-sm sm:leading-5">
       <Option
         v-for="item in state.collection"
@@ -69,5 +73,6 @@ export default {
         <slot :item="item.value" />
       </Option>
     </ul>
+    <button v-bind="dismissButtonProps" />
   </div>
 </template>
